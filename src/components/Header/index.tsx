1307c97a6587a2598610206de9cc1551959c40c4
alifsabrani <alifsabrani@gmail.com>
2020-07-28 20:07:06 +0800

    optimize Header for mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,48 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 import styles from "./style.module.css";
 
 export default function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const currentRoute = useLocation();
+  useEffect(() => {
+    setMenuIsOpen(false);
+  }, [currentRoute]);
   return (
-    <div className={styles.header}>
-      <NavLink className={styles.navLink} to="/">
-        <div className={styles.logo}>Alif Sabrani</div>
-      </NavLink>
-      <div className={styles.navs}>
-        <NavLink className={styles.navLink} to="/home">
-          Home
+    <div className={styles.navContainer}>
+      <div className={styles.header}>
+        <NavLink className={styles.navLink} to="/">
+          <div className={styles.logo}>Alif Sabrani</div>
         </NavLink>
-        <NavLink className={styles.navLink} to="/portofolio">
-          Portofolio
-        </NavLink>
-        <NavLink className={styles.navLink} to="/contact">
-          Contact
-        </NavLink>
+        <div className={styles.navs}>
+          <NavLink className={styles.navLink} to="/home">
+            Home
+          </NavLink>
+          <NavLink className={styles.navLink} to="/portofolio">
+            Portofolio
+          </NavLink>
+          <NavLink className={styles.navLink} to="/contact">
+            Contact
+          </NavLink>
+        </div>
+        <div className={styles.menuButton}>
+          <button onClick={() => setMenuIsOpen(!menuIsOpen)}>Open</button>
+        </div>
       </div>
+      {menuIsOpen && (
+        <div className={styles.mobileNav}>
+          <NavLink className={styles.navLink} to="/home">
+            Home
+          </NavLink>
+          <NavLink className={styles.navLink} to="/portofolio">
+            Portofolio
+          </NavLink>
+          <NavLink className={styles.navLink} to="/contact">
+            Contact
+          </NavLink>
+        </div>
+      )}
     </div>
   );
 }

78abe678f1bcb9b8cca23493374467c2af271cca
alifsabrani <alifsabrani@gmail.com>
2020-08-09 19:47:02 +0800

    add Paper component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import Container from "../../components/Container";
 import Row from "../../components/Row";
+import Paper from "../../components/Paper";
 
 import "../../styles/typography.module.css";
+import Col from "../../components/Col";
 
 export default function Home() {
   return (
-    <Container pageCenter={true}>
+    <Container>
       <Row align="center">
-        <h1>ALIF SABRANI</h1>
+        <Container pageCenter={true}>
+          <Row align="center">
+            <h1>ALIF SABRANI</h1>
+          </Row>
+          <Row align="center">
+            <p>Software Engineer</p>
+          </Row>
+        </Container>
       </Row>
       <Row align="center">
-        <p>Software Engineer</p>
+        <Container>
+          <Row align="left">
+            <Col size={12}>
+              <h2>ABOUT ME</h2>
+            </Col>
+          </Row>
+          <Row align="center">
+            <Col size={12}>
+              <Paper>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
+                  laborum soluta sequi impedit, voluptate vero quaerat fugiat
+                  magni quos quibusdam non minima. Temporibus, quas deleniti
+                  dignissimos aperiam cumque maxime atque.
+                </p>
+              </Paper>
+            </Col>
+          </Row>
+        </Container>
       </Row>
     </Container>
   );

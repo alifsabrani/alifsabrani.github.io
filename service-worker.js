9b3ed9150266f3f401272c363f90c64e878c5163
alifsabrani <alifsabrani@gmail.com>
2020-08-18 20:01:55 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.063d67ed13c8ff84b5d366190081aed4.js"
+  "/precache-manifest.6eea25a6fb56269729e8b3e1ac28c276.js"
 );
 
 self.addEventListener('message', (event) => {
